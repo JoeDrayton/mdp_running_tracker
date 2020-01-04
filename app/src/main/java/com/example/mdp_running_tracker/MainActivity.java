3a6d138fd8e0dc9ffc638eef95a4514f42aaba1c
JoeDrayton <34310811+JoeDrayton@users.noreply.github.com>
2020-01-04 17:40:10 +0000

    more gubbins

--- app/src/main/java/com/example/mdp_running_tracker/MainActivity.java
+++ app/src/main/java/com/example/mdp_running_tracker/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,6 +18,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions(INITIAL_PERMS, 1337);
         //journey.clear();
     }
 
@@ -57,6 +62,29 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void displayCoordinates(){
+        String[] columns = {
+                ContentContract._ID,
+                ContentContract.LATITUDE,
+                ContentContract.LONGITUDE
+        };
+        Cursor c = getContentResolver().query(ContentContract.COORDINATES_URI, columns, null, null, null);
+
+        String colsToDisplay[] = new String[]{
+                ContentContract._ID,
+                ContentContract.LATITUDE,
+                ContentContract.LONGITUDE
+        };
+        int[] colResIds = new int[]{
+                R.id._id,
+                R.id.latitude,
+                R.id.longitude
+        };
+        // Sets the list view adapter to the cursor and relevant columns
+        ListView lv = findViewById(R.id.coordinates);
+        lv.setAdapter(new SimpleCursorAdapter(this, R.layout.coordinates, c, colsToDisplay, colResIds, 0));
+        Log.d("g53mdp", DatabaseUtils.dumpCursorToString(c));
+    }
     public void onRecordActivity(View v){
         Button recorder = findViewById(R.id.recordButton);
         if(recording){
@@ -67,14 +95,16 @@ public class MainActivity extends AppCompatActivity {
                 coords.put(ContentContract.LATITUDE, current.latitude);
                 coords.put(ContentContract.LONGITUDE, current.longitude);
                 Log.d("g53mdp",current.latitude + ":" + current.longitude);
+                Uri result = getContentResolver().insert(ContentContract.COORDINATES_URI, coords);
+                result.toString();
             }
-            Uri result = getContentResolver().insert(ContentContract.COORDINATES_URI, coords);
-            result.toString();
+            displayCoordinates();
             recording = false;
         } else {
-
+            if(!journey.isEmpty()){
+                journey.clear();
+            }
             recorder.setText("Finish");
-            requestPermissions(INITIAL_PERMS, 1337);
             LocationManager locationManager =
                     (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             MyLocationListener locationListener = new MyLocationListener();
---
+++ app/src/main/java/com/example/mdp_running_tracker/RunViewer.java
@@ -0,0 +1,14 @@
+package com.example.mdp_running_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_viewer);
+    }
+}
